553d74d4a59d4b91edd1f986e0e9c2ee1f6e0fc4
Herick Prazeres <herick_prazeres@hotmail.com>
2024-12-06 16:18:22 -0300

    Implementando as lacunas do desafio de fundamentos .Net

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,16 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            
+            while(string.IsNullOrEmpty(placa))
+            {
+                Console.WriteLine("Placa não informada.");
+                Console.WriteLine("Por favor digite a placa do veículo para estacionar:");
+                placa = Console.ReadLine();
+            }
+                        
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
@@ -26,6 +36,7 @@ namespace DesafioFundamentos.Models
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+            placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -38,10 +49,33 @@ namespace DesafioFundamentos.Models
                 int horas = 0;
                 decimal valorTotal = 0; 
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                var inputHoras = Console.ReadLine();
+                if (inputHoras.All(char.IsDigit))
+                {
+                    horas = int.Parse(inputHoras.First().ToString());
+                }
+                else
+                {
+                    bool isDigit = false;
+                    while (!isDigit)
+                    {
+                        Console.WriteLine("A quantidade de horas deve ser número entre 1 e 12 horas.");
+                        inputHoras = Console.ReadLine();
+                        if (inputHoras.All(char.IsDigit))
+                        {
+                            horas = int.Parse(inputHoras.First().ToString());
+                            isDigit = true;
+                        }
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
+                    }
+                }                    
+
+                valorTotal = precoInicial + (precoPorHora * horas);
+
+                // TODO: Remover a placa digitada da lista de veículos
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: {valorTotal:C2}");
+                veiculos.Remove(placa);
             }
             else
             {
@@ -54,9 +88,18 @@ namespace DesafioFundamentos.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+                if(veiculos.Count() == 1)
+                    Console.WriteLine($"Há um total de {veiculos.Count()} veículo estacionado.");
+                else
+                    Console.WriteLine($"Há um total de {veiculos.Count()} veículos estacionados.");
+
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados                
+                foreach(var veiculo in veiculos)
+                {
+                    Console.WriteLine($"Placa: {veiculo}");
+                }
             }
             else
             {
